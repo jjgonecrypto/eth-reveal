7779f91b79a5ac7cb033dee6d43149ffd9e56ace
justin j. moses <justinjmoses@gmail.com>
2019-05-18 16:37:48 -0400

    Moving logging logic to CLI

--- reveal.js
+++ reveal.js
@@ -1,17 +1,35 @@
 #!/usr/bin/env node
-
 'use strict';
 
 const program = require('commander');
+const numbro = require('numbro');
+const { gray, red, yellow, green, cyan } = require('chalk');
 const { lookup } = require('./lib');
 
 // perform as CLI tool if args given
 if (require.main === module) {
-	require('pretty-error').start();
+	(async () => {
+		require('pretty-error').start();
 
-	program.option('-h, --hash <value>', 'Transaction hash (include 0x prefix)').parse(process.argv);
+		program
+			.option('-h, --hash <value>', 'Transaction hash (include 0x prefix)')
+			.parse(process.argv);
 
-	lookup({ hash: program.hash });
+		const { hash } = program;
+		console.log(gray('tx hash:', `https://etherscan.io/tx/${hash}`));
+
+		const { gasUsed, status, errorMessage, revertReason } = await lookup({ hash: program.hash });
+
+		console.log(gray(`Gas used: ${numbro(gasUsed).format('0,0')}`));
+		console.log(gray('Status:'), status > 0 ? green('success') : red('failure'));
+
+		if (errorMessage) {
+			console.log('ES error:', cyan(errorMessage));
+		}
+		if (revertReason) {
+			console.log('Revert reason:', yellow(revertReason));
+		}
+	})();
 }
 
 module.exports = lookup;
